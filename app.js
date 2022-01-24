4c0ca0fbfa5df85844ffc7081d5579c402033c86
AdriRamone90 <adriramone90@gmail.com>
2022-01-24 08:07:08 -0300

    practica 2

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal']*/
 
 let process = require("process");
 let texto = process.argv[2];
+let cantidad = process.argv[3]
 
-function miFuncion(texto){
+// function miFuncion(texto){
 
-    let nuevoArray = texto.split(" ");
+//     let nuevoArray = texto.split(" ");
 
-    return nuevoArray;
+//     return nuevoArray;
+// }
+
+// console.log(miFuncion(texto))
+
+/*Programa una función que repita un 
+texto X veces, pe. miFuncion('Hola Mundo', 3) 
+devolverá Hola Mundo Hola Mundo Hola Mundo.*/
+
+function miFuncion(texto, cantidad){
+    let stringTemp = texto + " "
+    let nuevoString = stringTemp.repeat(cantidad)
+    return nuevoString;
 }
 
-console.log(miFuncion(texto))
+console.log(miFuncion(texto, cantidad))
